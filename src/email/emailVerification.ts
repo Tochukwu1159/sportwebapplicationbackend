a6c7b3b82b645d2fbabe28e0c6e4063114f1d208
Tochukwu1159 <christinotochukwu@gmail.com>
2023-02-26 14:59:46 +0100

    updated send email module to named export

--- src/email/emailVerification.ts
+++ src/email/emailVerification.ts
@@ -35,3 +35,30 @@ export function forgotPasswordVerification(id: string): string {
 }
 
 
+import nodemailer from 'nodemailer';
+const forMailUser = process.env.GMAIL_USER as string;
+const forMailPass = process.env.GMAIL_PASS as string;
+const fromUser = process.env.FROM as string;
+const userSubject = process.env.SUBJECT as string;
+
+const transport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: forMailUser,
+    pass: forMailPass,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+export const sendEmail = (from:string, to:string, subject: string, html: string):Promise<unknown> => {
+    return new Promise((resolve, reject) => {
+      transport.sendMail(
+        { from: fromUser, subject: userSubject, to, html },
+        (err, info) => {
+          if (err) reject(err);
+          resolve(info);
+        },
+      );
+    });
+  }
